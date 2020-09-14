0acf72b45440d752d5e259c4d853271fd7705e06
Kenny Barbier <kenny@iMac-de-Kenny.local>
2020-09-14 19:15:08 +1100

    Couplage Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookdeva';
+  constructor(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyCXbnSq-0gbC8Lq6Yl8_cPY9jvVbRj3Nbs",
+      authDomain: "bookdeva-cd23c.firebaseapp.com",
+      databaseURL: "https://bookdeva-cd23c.firebaseio.com",
+      projectId: "bookdeva-cd23c",
+      storageBucket: "bookdeva-cd23c.appspot.com",
+      messagingSenderId: "538453868876",
+      appId: "1:538453868876:web:0f32789f290f9bfd4a0fe9",
+      measurementId: "G-QQM9Y224NR"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
