74e9b59f7a89c5f446987720109979233e12dd66
Kenny Barbier <kenny@iMac-de-Kenny.local>
2020-09-15 17:00:36 +1100

    auth

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import firebase = require('firebase');
+import { resolve } from 'url';
+import { reject } from 'q';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return new Promise(
+      resolve, reject) => {
+        firebase.auth().onAuthStateChanged(
+          (user) => {
+            if (user) {
+              resolve(true);
+            } else {
+              this.router.navigate(['/auth', 'signin']);
+              resolve(false);
+            }
+          }
+        );
+      }
+      );
+  }
 }
